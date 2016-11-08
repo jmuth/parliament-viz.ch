392d0f83df119d05aef71b393cc4b566c813f968
jmuth <joachim.h.muth@gmail.com>
2016-11-08 13:42:12 +0100

    beggining of class loader, not finnished

--- scraping/loader.py
+++ scraping/loader.py
@@ -8,114 +8,141 @@ import xml.etree.ElementTree as ET
 #
 # Distributed under terms of the MIT license.
 
-"""
 
-"""
-
-
-# Function to parse the data
-def get_and_parse(base_url):
+class Loader:
     """
-    Send GET request to parlament.ch and parse the return XML file to a pandas.data_frame
-    :param base_url: (str) GET url request
-    :return: (pandas.data_frame) parsed XML
-    """
-    print(base_url)
-    with urllib.request.urlopen(base_url) as url:
-        s = url.read()
-    root = ET.fromstring(s)
-
-    dict_ = {}
-    base = "{http://www.w3.org/2005/Atom}"
-    for child in root.iter(base + 'entry'):
-        for children in child.iter(base + 'content'):
-            for properties in children:
-                for subject in properties:
-                    # print(subject.text)
-                    s = subject.tag.split('}')
-                    if s[1] in dict_:
-                        dict_[s[1]].append(subject.text)
-                    else:
-                        dict_[s[1]] = [subject.text]
-    data = pd.DataFrame(dict_)
-    return data
-
-
-def party():
-    """
-    Load the Party table from parlament.ch
-    Remove useless column
-    :return: (pandas.data_frame) Party
-    """
-    # Url to get all the party
-    url_party = "https://ws.parlament.ch/odata.svc/Party?$filter=Language%20eq%20'FR'"
-    df_party = get_and_parse(url_party)
-
-    # Drop the Language Column
-    df_party = df_party.drop('Language', axis=1)
-
-    # Write file
-    df_party.to_csv('data/party.csv')
-
-    return df_party
-
-
-def person():
-    """
-    Load the Person table from parlament.ch
-    Remove useless field
-    :return: (pandas.data_frame) Person
+    Class containing all the method to properly download data from parlament.ch
     """
 
-    df_person = get_big_table('Person')
-    # Drop useless columns (for council)
-    # df_person = df_person.drop('BirthPlace_Canton', axis=1)
-    # df_person = df_person.drop('BirthPlace_City', axis=1)
+    def __init__(self):
+        self.tables = {'party': 'Party',
+                       'person': 'Person',
+                       'member_council': 'MemberCouncil',
+                       'council': 'Council'}
+        self.base_url = "https://ws.parlament.ch/odata.svc/"
 
-    # write file
-    df_person.to_csv('data/person.csv')
+    # Function to parse the data
+    def get_and_parse(base_url):
+        """
+        Send GET request to parlament.ch and parse the return XML file to a pandas.data_frame
+        :param base_url: (str) GET url request
+        :return: (pandas.data_frame) parsed XML
+        """
+        print(base_url)
+        with urllib.request.urlopen(base_url) as url:
+            s = url.read()
+        root = ET.fromstring(s)
 
-    return df_person
+        dict_ = {}
+        base = "{http://www.w3.org/2005/Atom}"
+        for child in root.iter(base + 'entry'):
+            for children in child.iter(base + 'content'):
+                for properties in children:
+                    for subject in properties:
+                        # print(subject.text)
+                        s = subject.tag.split('}')
+                        if s[1] in dict_:
+                            dict_[s[1]].append(subject.text)
+                        else:
+                            dict_[s[1]] = [subject.text]
+        data = pd.DataFrame(dict_)
+        return data
 
 
-def member_council():
-    table_name = 'MemberCouncil'
-    df_member_council = get_big_table(table_name)
-    df_member_council.to_csv('data/' + table_name.lower() + '.csv')
-    
-    return df_member_council
+    def party():
+        """
+        Load the Party table from parlament.ch
+        Remove useless column
+        :return: (pandas.data_frame) Party
+        """
+        # Url to get all the party
+        url_party = "https://ws.parlament.ch/odata.svc/Party?$filter=Language%20eq%20'FR'"
+        df_party = get_and_parse(url_party)
+
+        # Drop the Language Column
+        df_party = df_party.drop('Language', axis=1)
+
+        # Write file
+        df_party.to_csv('data/party.csv')
+
+        return df_party
 
 
-def get_big_table(table_name):
-    # url
-    base = "https://ws.parlament.ch/odata.svc/"
-    table_name += "?"
-    language = "$filter=Language%20eq%20'FR'"
-    id_from = "ID%20ge%20"
-    id_to = "%20and%20ID%20lt%20"
+    def person():
+        """
+        Load the Person table from parlament.ch
+        Remove useless field
+        :return: (pandas.data_frame) Person
+        """
 
-    # loop parameters
-    limit_api = 1000
-    data_frames = []
-    i = 0
-    time_out = 0
-    while True:
-        url = base + table_name + language + '%20and%20' + id_from + str(i) + id_to + str(i + limit_api)
+        df_person = get_big_table('Person')
+        # Drop useless columns (for council)
+        # df_person = df_person.drop('BirthPlace_Canton', axis=1)
+        # df_person = df_person.drop('BirthPlace_City', axis=1)
 
-        df = get_and_parse(url)
+        # write file
+        df_person.to_csv('data/person.csv')
 
-        # stop when we reach the end of the data
-        if df.shape == (0, 0):
-            break
+        return df_person
 
-        # stop after 10 iteration to avoid swiss police to knock at our door
-        if time_out > 10:
-            print("Loader timed out after ", time_out, " iterations. Data frame IDs are greater than ", i)
-            break
 
-        data_frames.append(df)
-        i += limit_api
-        time_out += 1
+    def member_council():
+        table_name = 'MemberCouncil'
+        df_member_council = get_big_table(table_name)
+        df_member_council.to_csv('data/' + table_name.lower() + '.csv')
 
-    df = pd.concat(data_frames)
-    return df
+        return df_member_council
+
+
+    def council():
+        url = "https://ws.parlament.ch/odata.svc/Council?$top=1000&$filter=Language eq 'FR'"
+        return get_and_parse(url)
+
+
+    def get_big_table(table_name):
+        """
+        Loop URL request on table by step of 1000 id's and load data until reaches the end
+        Time Out after 10 iterations
+        :param table_name: Name of the wished table
+        :return: (pandas.data_frame) table
+        """
+        # url
+        base = "https://ws.parlament.ch/odata.svc/"
+        table_name += "?"
+        language = "$filter=Language%20eq%20'FR'"
+        id_from = "ID%20ge%20"
+        id_to = "%20and%20ID%20lt%20"
+
+        # loop parameters
+        limit_api = 1000
+        data_frames = []
+        i = 0
+        time_out = 0
+        while True:
+            url = base + table_name + language + '%20and%20' + id_from + str(i) + id_to + str(i + limit_api)
+
+            df = get_and_parse(url)
+
+            # stop when we reach the end of the data
+            if df.shape == (0, 0):
+                break
+
+            # stop after 10 iteration to avoid swiss police to knock at our door
+            if time_out > 10:
+                print("Loader timed out after ", time_out, " iterations. Data frame IDs are greater than ", i)
+                break
+
+            data_frames.append(df)
+            i += limit_api
+            time_out += 1
+
+        df = pd.concat(data_frames)
+        return df
+
+    def count(table_name):
+        url = "https://ws.parlament.ch/odata.svc/" + table_name + "/$count?$filter=Language%20eq%20'FR'"
+        with urllib.request.urlopen(url) as response:
+            n = response.read()
+        # get the number from the bytes
+        n = int(str(n).split("'")[1])
+        return n
