3f71c358a38f24a37423478b98d60d0106dd07cd
glederrey <gael.lederrey@epfl.ch>
2016-11-15 18:46:46 +0100

    [ADD] Portraits of the council members + [ADD] more clustering

--- viz/viz_script.js
+++ viz/viz_script.js
@@ -1,21 +1,46 @@
+// Get the clustering
+var rad = document.Clustering.buttons;
+var prev = null;
+var clustering = "none";
+for(var i = 0; i < rad.length; i++) {
+    rad[i].onclick = function() {
+        (prev)? console.log(prev.value):null;
+        if(this !== prev) {
+            prev = this;
+            clustering = this.value;
+            console.log(clustering);
+        }
+    };
+}
+
+// Define some variables
 var radius = 7,
     padding = 1;
-
-var tooltip = d3.select("div#councilor")
-    //.style("position", "absolute")
-    //.style("z-index", "10")
-    .style("visibility", "hidden");
 
 // We consider that the size of the box is 960x600
 var width = 960,
     height = 600;
 
 // Foci
-var foci = [
-                {x: 0.2*width, y:0.6*height}, // Foci CN
-                {x: 0.6*width, y: 0.2*height}, // Foci CE
-                {x: 0.8*width, y: 0.6*height}  // Foci CF
-            ];
+var foci = {
+    "council": {
+        "CN": {x: 0.2 * width, y: 0.6 * height},    // Foci CN
+        "CE": {x: 0.6 * width, y: 0.2 * height},    // Foci CE
+        "CF": {x: 0.8 * width, y: 0.6 * height}     // Foci CF
+    },
+    "none": {},                                     // No Foci
+    "parties": {},                                  // Foci are created later
+    "gender": {
+        "m": {x: 0.3*width, y: 0.5*height},         // Foci Male
+        "f": {x: 0.7*width, y: 0.5*height}          // Foci Female
+    },
+    "language": {}                                  // Foci are created later
+};
+
+var nbr_by_council = {};
+var nbr_by_party = {};
+var nbr_by_gender = {};
+var nbr_by_language = {};
 
 var svg = d3.select("div#viz")
         .append("div")
@@ -27,10 +52,9 @@ var svg = d3.select("div#viz")
         //class to make it responsive
         .classed("svg-content-responsive", true);
 
-
-
 var simulation = d3.forceSimulation().alphaDecay(0)
-        .force("collision", d3.forceCollide().radius(radius+padding))
+        .velocityDecay(0.1)
+        .force("collision", d3.forceCollide().radius(radius+2*padding).iterations(2))
     //.force("link", d3.forceLink().id(function(d) { return d.id; }))
     //.force("charge", d3.forceManyBody().strength(-2));
     //.force("center", d3.forceCenter(width / 2, height / 2));
@@ -41,9 +65,57 @@ d3.json("data/active.json", function(error, graph) {
 
     var nodes = graph.nodes;
 
+    var list_parties = [];
+    var list_languages = [];
+
     for(var i=0; i<nodes.length; i++) {
         nodes[i]["x"] = Math.random()*width;
         nodes[i]["y"] = Math.random()*height;
+        nodes[i]["radius"] = radius;
+
+        // Get nbr by council
+        if(!(nodes[i]["CouncilAbbreviation"] in nbr_by_council)) {
+            nbr_by_council[nodes[i]["CouncilAbbreviation"]] = 1;
+        } else {
+            nbr_by_council[nodes[i]["CouncilAbbreviation"]] += 1;
+        }
+
+        // Get nbr by party
+        if(!(nodes[i]["PartyAbbreviation"] in nbr_by_party)) {
+            nbr_by_party[nodes[i]["PartyAbbreviation"]] = 1;
+            list_parties.push(nodes[i]["PartyAbbreviation"]);
+        } else {
+            nbr_by_party[nodes[i]["PartyAbbreviation"]] += 1;
+        }
+
+        // Get nbr by gender
+        if(!(nodes[i]["GenderAsString"] in nbr_by_council)) {
+            nbr_by_gender[nodes[i]["GenderAsString"]] = 1;
+        } else {
+            nbr_by_gender[nodes[i]["GenderAsString"]] += 1;
+        }
+
+        // Get nbr by gender
+        if(!(nodes[i]["NativeLanguage"] in nbr_by_language)) {
+            nbr_by_language[nodes[i]["NativeLanguage"]] = 1;
+            list_languages.push(nodes[i]["NativeLanguage"]);
+        } else {
+            nbr_by_language[nodes[i]["NativeLanguage"]] += 1;
+        }
+    }
+
+    // Create Foci for parties
+    shuffle(list_parties);
+
+    for(var i=0; i<list_parties.length; i++) {
+        foci["parties"][list_parties[i]] = {"x": (i+1)/(list_parties.length+1)*width, "y": (Math.pow(-1, i)*0.25 + 0.5)*height};
+    }
+
+    // Create Foci for languages
+    shuffle(list_languages);
+
+    for(var i=0; i<list_languages.length; i++) {
+        foci["language"][list_languages[i]] = {"x": (i+1)/(list_languages.length+1)*width, "y": (Math.pow(-1, i)*0.25 + 0.5)*height};
     }
 
     var node = svg.append("g")
@@ -82,21 +154,28 @@ d3.json("data/active.json", function(error, graph) {
     }
 
     function emphasis(d) {
-        d3.select(this).style("r", radius+2);
-        tooltip.html(
-            "<div><b>Name: </b>" + d.FirstName + " " + d.LastName + "</div>" +
-            "<div><b>Party: </b>" + d.PartyName + " (" + d.PartyAbbreviation + ")</div>" +
-            "<div><b>Council: </b>" + d.CouncilName + "</div>" +
-            "<div><b>Birthday: </b>" + d.DateOfBirth + "</div>" +
-            "<img src=\"data/portraits/" + d.PersonIdCode + ".jpg\" alt=\"" +
-            d.FirstName + " " + d.LastName + "\">"
-        )
-        return tooltip.style("visibility", "visible");
+        d3.selectAll("circle").style("r", radius);
+        d3.select(this).style("r", 1.5*radius);
+        document.getElementById("councilorName").innerHTML = d.FirstName + " " + d.LastName;
+        document.getElementById("councilorParty").innerHTML = d.PartyName;
+        document.getElementById("councilorCouncil").innerHTML = d.CouncilName;
+        document.getElementById("councilorBirthday").innerHTML = d.DateOfBirth;
+        document.getElementById("councilorLanguage").innerHTML = d.NativeLanguage;
+        document.getElementById("councilorImage").src = "data/portraits/" + d.PersonIdCode + ".jpg";
+        document.getElementById("councilorImage").alt = d.FirstName + " " + d.LastName;
     }
 
+    window.addEventListener('click', function (evt) {
+        if (evt.detail === 3) {
+            nodes.forEach(function(o, i) {
+                o.x += (Math.random() - .5) * 40;
+                o.y += (Math.random() - .5) * 40;
+            });
+        }
+    });
+
     function back_to_normal(d) {
-        d3.select(this).style("r", radius);
-        return tooltip.style("visibility", "hidden");
+        //d3.select(this).style("r", radius);
     }
 
     function dbclick() {
@@ -150,33 +229,29 @@ function colors_parties(abbrev) {
 function gravity() {
     return function(d) {
         var alpha,
-            rad,
             foci_x,
             foci_y;
 
-        if (d.CouncilAbbreviation == "CN") {
-            alpha = 0.01;
-            rad = get_max_radius(200, radius+padding)+1;
-            foci_x = foci[0].x;
-            foci_y = foci[0].y;
-        } else if (d.CouncilAbbreviation == "CE") {
-            alpha = 0.03;
-            rad = get_max_radius(46, radius+padding)+1;
-            foci_x = foci[1].x;
-            foci_y = foci[1].y;
-        } else {
-            alpha = 0.05;
-            rad = get_max_radius(7, radius+padding)+1;
-            foci_x = foci[2].x;
-            foci_y = foci[2].y;
+        if(clustering == "council") {
+            foci_x = foci[clustering][d.CouncilAbbreviation].x;
+            foci_y = foci[clustering][d.CouncilAbbreviation].y;
+        } else if (clustering == "none") {
+            // Find a way such that the points goes to a random location
+            foci_x = d.x;
+            foci_y = d.y;
+        } else if (clustering == "parties") {
+            foci_x = foci[clustering][d.PartyAbbreviation].x;
+            foci_y = foci[clustering][d.PartyAbbreviation].y;
+        } else if (clustering == "gender") {
+            foci_x = foci[clustering][d.GenderAsString].x;
+            foci_y = foci[clustering][d.GenderAsString].y;
+        } else if (clustering == "language") {
+            foci_x = foci[clustering][d.NativeLanguage].x;
+            foci_y = foci[clustering][d.NativeLanguage].y;
         }
-
-        var dist = Math.sqrt(Math.pow(foci_y - d.y, 2) + Math.pow(foci_x - d.x, 2));
-
-        if (dist > rad) {
-            d.y += (foci_y - d.y) * alpha;
-            d.x += (foci_x - d.x) * alpha;
-        }
+        alpha = 0.05;
+        d.y += (foci_y - d.y) * alpha;
+        d.x += (foci_x - d.x) * alpha;
     };
 }
 
@@ -195,24 +270,9 @@ function dragended(d) {
     if (!d3.event.active) simulation.alphaTarget(0);
 }
 
-function get_max_radius(n, rad) {
-    if (n==1) {
-        return rad;
-    } else if (n<=7) {
-        return 2*rad;
-    } else if (n<=19) {
-        return 3*rad;
-    } else if (n<=37) {
-        return 4*rad;
-    } else if (n<=61) {
-        return 5*rad;
-    } else if (n<=91) {
-        return 6*rad;
-    } else if (n<=127) {
-        return 7*rad;
-    } else if (n<=169) {
-        return 8*rad;
-    } else if (n<=217) {
-        return 9*rad;
+function shuffle(a) {
+    for (var i = a.length; i; i--) {
+        var j = Math.floor(Math.random() * i);
+        [a[i - 1], a[j]] = [a[j], a[i - 1]];
     }
 }
